405ac62c2af85e41853d9d30a0e468469395fc50
Shamkumar <nothuman1209@gmail.com>
2024-06-20 12:53:23 +0800

    Added screen page

--- TT9L-04/dashboard.py
+++ TT9L-04/dashboard.py
@@ -2,3 +2,11 @@ import tkinter as tk
 from calculator import BudgetCalculator
 from bill_manager import BillManagerApp
 from loan_calculator import LoanCalculator
+
+class Dashboard(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.master = master
+        self.master.title("Dashboard")
+        self.master.geometry("400x300")
