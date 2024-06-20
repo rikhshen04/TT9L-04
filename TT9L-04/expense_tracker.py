b6b951e44ac91806f9a2d9fa667f10f9343ab42f
Shamkumar <nothuman1209@gmail.com>
2024-06-20 13:01:55 +0800

    Added screen settings and database file

--- TT9L-04/expense_tracker.py
+++ TT9L-04/expense_tracker.py
@@ -2,3 +2,16 @@ import tkinter as tk
 from tkinter import messagebox
 import os
 from dashboard import Dashboard
+
+class ExpenseTrackerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Expense Tracker App")
+        self.root.geometry("500x400")
+
+        self.large_font = ('Helvetica', 16)
+
+        self.database_file = "logindatabase.txt"
+        if not os.path.exists(self.database_file):
+            with open(self.database_file, "w") as file:
+                file.write("")
