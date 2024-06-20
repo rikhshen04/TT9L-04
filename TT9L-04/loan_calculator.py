5a18d5a8b1f4c9dd8a6157ccb682fa6e36d43bab
Shamkumar <nothuman1209@gmail.com>
2024-06-20 13:18:24 +0800

    Added screen settings

--- TT9L-04/loan_calculator.py
+++ TT9L-04/loan_calculator.py
@@ -1,3 +1,12 @@
 # loan_calculator.py
 import tkinter as tk
 from tkinter import ttk
+
+class LoanCalculator:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Calculate Loan Interest")
+        self.master.geometry("500x500")
+        self.master.configure(bg='#2e2e2e')
+
+        self.create_widgets()
