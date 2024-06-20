7bfe6fe9d1bb2029db45a39a4542f60ab3308e31
Shamkumar <nothuman1209@gmail.com>
2024-06-20 12:11:29 +0800

    Added screen settings

--- TT9L-04/bill_manager.py
+++ TT9L-04/bill_manager.py
@@ -1,3 +1,10 @@
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime, timedelta
+
+class BillManagerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Upcoming Bills and Payments")
+        self.root.geometry("600x400")
+        self.root.configure(bg="#e9ecef")
