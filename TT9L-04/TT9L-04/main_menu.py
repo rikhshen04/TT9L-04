857e6f2a24a6a93fbf3625f3e99846cbfbd9b8a2
Avaneshkumar <rgavaneshkumar05@gmail.com>
2024-06-22 22:42:40 +0800

    Budgetcalculator

--- TT9L-04/TT9L-04/main_menu.py
+++ TT9L-04/TT9L-04/main_menu.py
@@ -4,3 +4,9 @@ import os
 from calculator import BudgetCalculator
 from bill_manager import BillManagerApp
 from loan_calculator import LoanCalculator
+
+class ExpenseTrackerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Expense Tracker App")
+        self.root.geometry("600x500")
